48327545194467ba782ecaae47a499b2fb7a201c
Anil Yadav <anilyadav8149838058@gmail.com>
2025-08-05 10:45:46 +0530

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend:{
+      fontFamily: {
+        sans: ['"Akshar"', 'sans-serif'], // Override default sans
+      },
+    },
   },
   plugins: [],
 }
